be9c87037298b3e8699b3dcd596647f85d8cff23
Marcin Krolik <marcin.krolik@intel.com>
2016-03-18 15:04:25 +0100

    Publisher implementation completed

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# snap-plugin-publisher-kairosdb
+# snap publisher plugin - KairosDB 
+
+This plugin supports pushing metrics into an KairosDB instance.
+
+It's used in the [snap framework](http://github.com/intelsdi-x/snap).
+
+1. [Getting Started](#getting-started)
+  * [System Requirements](#system-requirements)
+  * [Installation](#installation)
+  * [Configuration and Usage](#configuration-and-usage)
+2. [Documentation](#documentation)
+  * [Collected Metrics](#collected-metrics)
+  * [Examples](#examples)
+  * [Roadmap](#roadmap)
+3. [Community Support](#community-support)
+4. [Contributing](#contributing)
+5. [License](#license)
+6. [Acknowledgements](#acknowledgements)
+
+## Getting Started
+
+### System Requirements
+
+* [golang 1.4+](https://golang.org/dl/)
+
+Support Matrix
+
+- KairosDB V1 REST API
+
+### Installation
+
+#### Download InfluxDB plugin binary:
+You can get the pre-built binaries for your OS and architecture at snap's [GitHub Releases](https://github.com/intelsdi-x/snap/releases) page.
+
+#### To build the plugin binary:
+Fork https://github.com/intelsdi-x/snap-plugin-publisher-kairosdb  
+Clone repo into `$GOPATH/src/github.com/intelsdi-x/`:
+
+```
+$ git clone https://github.com/<yourGithubID>/snap-plugin-publisher-kairosdb.git
+```
+
+Build the plugin by running make within the cloned repo:
+```
+$ make
+```
+This builds the plugin in `/build/rootfs/`
+
+### Configuration and Usage
+* Set up the [snap framework](https://github.com/intelsdi-x/snap/blob/master/README.md#getting-started)
+* Ensure `$SNAP_PATH` is exported  
+`export SNAP_PATH=$GOPATH/src/github.com/intelsdi-x/snap/build`
+
+## Documentation
+<< @TODO
+
+### Examples
+<< @TODO
+
+### Roadmap
+
+There isn't a current roadmap for this plugin, but it is in active development. As we launch this plugin, we do not have any outstanding requirements for the next release.
+
+If you have a feature request, please add it as an [issue](https://github.com/intelsdi-x/snap-plugin-publisher-kairosdb/issues/new) and/or submit a [pull request](https://github.com/intelsdi-x/snap-plugin-publisher-kairosdb/pulls).
+
+## Community Support
+This repository is one of **many** plugins in **snap**, a powerful telemetry framework. See the full project at http://github.com/intelsdi-x/snap To reach out to other users, head to the [main framework](https://github.com/intelsdi-x/snap#community-support)
+
+## Contributing
+We love contributions! 
+
+There's more than one way to give back, from examples to blogs to code updates. See our recommended process in [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## License
+[snap](http://github.com/intelsdi-x/snap), along with this plugin, is an Open Source software released under the Apache 2.0 [License](LICENSE).
+
+## Acknowledgements
+* Author: [Marcin Krolik](https://github.com/marcin-krolik)
+
+And **thank you!** Your contribution, through code and participation, is incredibly important to us.
---
+++ publisher/publisher.go
@@ -0,0 +1,212 @@
+/*
+http://www.apache.org/licenses/LICENSE-2.0.txt
+Copyright 2016 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package publisher
+
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core/ctypes"
+
+	"github.com/intelsdi-x/snap-plugin-publisher-kairosdb/kairos"
+)
+
+const (
+	name        = "kairos"
+	version     = 1
+	pluginType  = plugin.PublisherPluginType
+	publishPath = "/api/v1/datapoints"
+)
+
+// Meta returns a plugin meta data
+func Meta() *plugin.PluginMeta {
+	return plugin.NewPluginMeta(
+		name,
+		version,
+		pluginType,
+		[]string{plugin.SnapGOBContentType},
+		[]string{plugin.SnapGOBContentType},
+	)
+}
+
+// New returns an instance of the KairosDB publisher
+func New() *publisher {
+	return &publisher{}
+}
+
+func (pub *publisher) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
+	cp := cpolicy.New()
+	config := cpolicy.NewPolicyNode()
+
+	r1, err := cpolicy.NewStringRule("host", true)
+	if err != nil {
+		return nil, err
+	}
+	r1.Description = "KairosDB host"
+	config.Add(r1)
+
+	r2, err := cpolicy.NewIntegerRule("port", true)
+	if err != nil {
+		return nil, err
+	}
+	r2.Description = "KairosDB port"
+	config.Add(r2)
+
+	cp.Add([]string{""}, config)
+	return cp, nil
+}
+
+// Publish publishes metric data to Kairosdb
+func (pub *publisher) Publish(contentType string, content []byte, config map[string]ctypes.ConfigValue) error {
+	logger := getLogger(config)
+	var metrics []plugin.PluginMetricType
+
+	// decode content to metrics type
+	switch contentType {
+	case plugin.SnapGOBContentType:
+		dec := gob.NewDecoder(bytes.NewBuffer(content))
+		if err := dec.Decode(&metrics); err != nil {
+			logger.WithFields(log.Fields{
+				"err": err,
+			}).Error("decoding error")
+			return err
+		}
+	default:
+		logger.Errorf("unknown content type '%v'", contentType)
+		return fmt.Errorf("Unknown content type '%s'", contentType)
+	}
+
+	// translate metrics to KairosDB publishing format
+	points := []kairos.DataPoint{}
+	for _, metric := range metrics {
+		point := kairos.DataPoint{
+			Name:      strings.Join(metric.Namespace(), "/"),
+			Value:     metric.Data(),
+			TimeStamp: metric.Timestamp().Unix(),
+			Tags:      metric.Tags(),
+		}
+		points = append(points, point)
+	}
+
+	// serialization
+	rendered, err := json.Marshal(points)
+	if err != nil {
+		logger.WithFields(log.Fields{
+			"err": err,
+		}).Error("Serialization error")
+		return err
+	}
+
+	// prepare publishing request
+	u, err := url.Parse(
+		fmt.Sprintf("http://%s:%d%s",
+			config["host"].(ctypes.ConfigValueStr).Value,
+			config["port"].(ctypes.ConfigValueInt).Value,
+			publishPath,
+		),
+	)
+	req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(rendered))
+	req.Header.Set("Content-Type", "application/json")
+
+	// send request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logger.WithFields(log.Fields{
+			"err": err,
+		}).Error("Request error")
+		return err
+	}
+	defer resp.Body.Close()
+
+	// check response status
+	if resp.StatusCode != http.StatusNoContent {
+		logger.WithFields(log.Fields{
+			"err": err,
+		}).Error("Response error ", resp.StatusCode)
+		return err
+	}
+
+	return nil
+}
+
+type publisher struct{}
+
+func getLogger(config map[string]ctypes.ConfigValue) *log.Entry {
+	logger := log.WithFields(log.Fields{
+		"plugin-name":    name,
+		"plugin-version": version,
+		"plugin-type":    pluginType.String(),
+	})
+
+	// default
+	log.SetLevel(log.WarnLevel)
+
+	if debug, ok := config["debug"]; ok {
+		switch v := debug.(type) {
+		case ctypes.ConfigValueBool:
+			if v.Value {
+				log.SetLevel(log.DebugLevel)
+				return logger
+			}
+		default:
+			logger.WithFields(log.Fields{
+				"field":         "debug",
+				"type":          v,
+				"expected type": "ctypes.ConfigValueBool",
+			}).Error("invalid config type")
+		}
+	}
+
+	if loglevel, ok := config["log-level"]; ok {
+		switch v := loglevel.(type) {
+		case ctypes.ConfigValueStr:
+			switch strings.ToLower(v.Value) {
+			case "warn":
+				log.SetLevel(log.WarnLevel)
+			case "error":
+				log.SetLevel(log.ErrorLevel)
+			case "debug":
+				log.SetLevel(log.DebugLevel)
+			case "info":
+				log.SetLevel(log.InfoLevel)
+			default:
+				log.WithFields(log.Fields{
+					"value":             strings.ToLower(v.Value),
+					"acceptable values": "warn, error, debug, info",
+				}).Warn("invalid config value")
+			}
+		default:
+			logger.WithFields(log.Fields{
+				"field":         "log-level",
+				"type":          v,
+				"expected type": "ctypes.ConfigValueStr",
+			}).Error("invalid config type")
+		}
+	}
+
+	return logger
+}
