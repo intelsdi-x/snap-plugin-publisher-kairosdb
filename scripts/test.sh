80be18c4d0181c77db11556a6d123ab9976d7d33
katarzyna-z <katarzyna.zabrocka@intel.com>
2016-04-15 10:49:31 +0200

    Added support for go 1.6 in travis tests and removed getting of go vet from test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -31,7 +31,6 @@ TEST_SUITE=$1
 if [[ $TEST_SUITE == "unit" ]]; then
 	go get github.com/axw/gocov/gocov
 	go get github.com/mattn/goveralls
-	go get golang.org/x/tools/cmd/vet
 	go get golang.org/x/tools/cmd/goimports
 	go get github.com/smartystreets/goconvey/convey
 	go get github.com/smartystreets/assertions
